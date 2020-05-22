74a805ca9728239a33d1dfbb990604d43a8d41be
James Davis <james.norman.davis@gmail.com>
2020-05-22 12:26:33 +1000

    Move logger to lib

--- app.js
+++ app.js
@@ -1,35 +1,10 @@
 const config = require('./config')
 const gdrive = require('./lib/gdrive')
 const zoom = require('./lib/zoom')
+const logger = require('./lib/logger')
 const fs = require('fs')
 const filenamify = require('filenamify')
 const path = require('path')
-const { createLogger, format, transports } = require('winston');
-
-// Initialise the logger
-const myFormat = format.printf(({ level, message, timestamp }) => {
-    return `${timestamp} ${level}: ${message}`
-})
-const logger = createLogger({
-  level: 'info',
-  format: format.combine(
-    format.timestamp({
-      format: 'YYYY-MM-DD HH:mm:ss'
-    }),
-    format.errors({ stack: true }),
-    format.splat(),
-    format.prettyPrint(),
-    myFormat
-  ),
-  defaultMeta: { service: 'zoom-archiver' },
-  transports: [
-    // - Write to all logs with level `info` and below to `quick-start-combined.log`.
-    // - Write all logs error (and below) to `quick-start-error.log`.
-    new transports.File({ filename: 'logs/zoom-archiver-error.log', level: 'error' }),
-    new transports.File({ filename: 'logs/zoom-archiver-combined.log' }),
-    new transports.Console({ level: 'info' })
-  ]
-});
 
 
 async function start() {
